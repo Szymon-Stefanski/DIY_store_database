598d7a61b0823f17b3e30e691cbe337a73e291e3
Szymon Stefański <szymonstefanski1@gmail.com>
2024-10-23 17:37:14 +0200

    Update packages

--- PACKAGES/pkg_student_management.sql
+++ PACKAGES/pkg_student_management.sql
@@ -1,3 +1,4 @@
+-- PACKAGE FOR STUDENT MANAGEMENT OPERATIONS
 CREATE OR REPLACE PACKAGE pkg_student_management 
 IS
   PROCEDURE add_new_student(    
@@ -11,14 +12,22 @@ IS
     v_phone_number  students.phone_number%TYPE,
     v_pesel         students.PESEL%TYPE
     );
-  --PROCEDURE delete_student_record;
-  --PROCEDURE update_student_address;
-  FUNCTION get_student_info(p_student_id students.student_id%TYPE) RETURN VARCHAR2;
+  PROCEDURE delete_student_record(v_student_id students.student_id%TYPE);
+  PROCEDURE update_student_address(
+    v_student_id    students.student_id%TYPE,
+    v_city          students.city%TYPE,
+    v_street        students.street%TYPE,
+    v_home_number   students.home_number%TYPE,
+    v_postal_code   students.postal_code%TYPE
+    );
+  FUNCTION get_student_info(v_student_id students.student_id%TYPE) RETURN VARCHAR2;
 END pkg_student_management;
 /
 
+
 CREATE OR REPLACE PACKAGE BODY pkg_student_management 
 IS
+  -- PROCEDURE TO ADD A NEW STUDENT RECORD
   PROCEDURE add_new_student(    
     v_first_name    students.first_name%TYPE,
     v_last_name     students.last_name%TYPE,
@@ -29,16 +38,16 @@ IS
     v_postal_code   students.postal_code%TYPE,
     v_phone_number  students.phone_number%TYPE,
     v_pesel         students.PESEL%TYPE
-    )
+  )
   IS
   BEGIN
     INSERT INTO 
     students (first_name, last_name, email, city, street, home_number, postal_code, phone_number, PESEL)
     VALUES 
     (v_first_name, v_last_name, v_email, v_city, v_street, v_home_number, v_postal_code, v_phone_number, v_pesel);
-    
+
     DBMS_OUTPUT.PUT_LINE('Student added successfully.');
-    
+
   EXCEPTION
     WHEN DUP_VAL_ON_INDEX THEN
       DBMS_OUTPUT.PUT_LINE('Error: A student with this phone number or PESEL already exists.');
@@ -46,7 +55,59 @@ IS
       DBMS_OUTPUT.PUT_LINE('Error occurred. SQLCODE: ' || SQLCODE || ', SQLERRM: ' || SQLERRM);
   END add_new_student;
 
-  FUNCTION get_student_info(p_student_id students.student_id%TYPE) 
+
+
+  -- PROCEDURE TO DELETE A STUDENT RECORD BY STUDENT ID
+  PROCEDURE delete_student_record(v_student_id students.student_id%TYPE)
+  IS
+  BEGIN
+    DELETE FROM students WHERE student_id = v_student_id;
+
+    IF SQL%ROWCOUNT = 0 THEN
+      DBMS_OUTPUT.PUT_LINE('Student not found.');
+    ELSE
+      DBMS_OUTPUT.PUT_LINE('Student deleted successfully.');
+    END IF;
+
+  EXCEPTION
+    WHEN OTHERS THEN
+      DBMS_OUTPUT.PUT_LINE('Error occurred. SQLCODE: ' || SQLCODE || ', SQLERRM: ' || SQLERRM);
+  END delete_student_record;
+
+
+
+  -- PROCEDURE TO UPDATE A STUDENT'S ADDRESS
+  PROCEDURE update_student_address(
+    v_student_id    students.student_id%TYPE,
+    v_city          students.city%TYPE,
+    v_street        students.street%TYPE,
+    v_home_number   students.home_number%TYPE,
+    v_postal_code   students.postal_code%TYPE
+  )
+  IS
+  BEGIN
+    UPDATE students 
+    SET city = v_city,
+        street = v_street,
+        home_number = v_home_number,
+        postal_code = v_postal_code
+    WHERE student_id = v_student_id;
+
+    IF SQL%ROWCOUNT = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Student not found.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Student address updated successfully.');
+    END IF;
+
+  EXCEPTION
+    WHEN OTHERS THEN
+      DBMS_OUTPUT.PUT_LINE('Error occurred. SQLCODE: ' || SQLCODE || ', SQLERRM: ' || SQLERRM);
+  END update_student_address;
+
+
+
+  -- FUNCTION TO RETRIEVE STUDENT INFORMATION BY STUDENT ID
+  FUNCTION get_student_info(v_student_id students.student_id%TYPE) 
   RETURN VARCHAR2
   IS
     v_first_name    students.first_name%TYPE;
@@ -64,9 +125,9 @@ IS
     SELECT first_name, last_name, email, city, street, home_number, postal_code, phone_number, PESEL
     INTO v_first_name, v_last_name, v_email, v_city, v_street, v_home_number, v_postal_code, v_phone_number, v_pesel
     FROM students
-    WHERE student_id = p_student_id;
+    WHERE student_id = v_student_id;
 
-    v_output := 'Student ID: ' || p_student_id || ', ' ||
+    v_output := 'Student ID: ' || v_student_id || ', ' ||
                 'First Name: ' || v_first_name || ', ' ||
                 'Last Name: ' || v_last_name || ', ' ||
                 'Email: ' || v_email || ', ' ||
@@ -78,7 +139,7 @@ IS
                 'PESEL: ' || v_pesel;
 
     RETURN v_output;
-  
+
   EXCEPTION
     WHEN NO_DATA_FOUND THEN
       RETURN 'Student not found.';
