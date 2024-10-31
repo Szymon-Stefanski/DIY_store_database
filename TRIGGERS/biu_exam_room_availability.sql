0d62f3fe0662de654a48a1fcc6ad81573f0b7d24
Szymon Stefański <szymonstefanski1@gmail.com>
2024-10-31 20:05:52 +0100

    Update triggers - adding trigger for exam table room availability

---
+++ TRIGGERS/biu_exam_room_availability.sql
@@ -0,0 +1,18 @@
+-- This trigger ensures that an exam cannot be assigned to room that are already booked.
+CREATE OR REPLACE TRIGGER biu_exam_room_availability
+BEFORE INSERT OR UPDATE ON exams
+FOR EACH ROW
+DECLARE
+    v_conflict_count NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_conflict_count
+    FROM exams
+    WHERE room_id = :NEW.room_id
+    AND exam_date = :NEW.exam_date
+    AND exam_id != :NEW.exam_id;
+
+    IF v_conflict_count > 0 THEN
+        RAISE_APPLICATION_ERROR(-2007, 'The room is already booked for another exam at this time.');
+    END IF;
+END;
+/
