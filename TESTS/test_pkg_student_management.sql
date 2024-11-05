766171e1116c10c24aae9bbdce29fd6703a53e7b
Szymon Stefański <szymonstefanski1@gmail.com>
2024-11-05 23:36:18 +0100

    Update TESTS - adding test to check deleting student record procedure

--- TESTS/test_pkg_student_management.sql
+++ TESTS/test_pkg_student_management.sql
@@ -4,10 +4,14 @@ CREATE OR REPLACE PACKAGE test_pkg_student_management IS
   
   --%test test_add_student_success
   PROCEDURE test_add_student_success;
+
+  --%test test_delete_student_success
+  PROCEDURE test_delete_student_success;
 END test_pkg_student_management;
 /
 
 CREATE OR REPLACE PACKAGE BODY test_pkg_student_management IS
+  --TEST TO ADD A NEW STUDENT RECORD
   PROCEDURE test_add_student_success IS
     v_student_count_before NUMBER;
     v_student_count_after NUMBER;
@@ -49,5 +53,47 @@ CREATE OR REPLACE PACKAGE BODY test_pkg_student_management IS
 
     DELETE FROM NEO.students WHERE phone_number = '123456789' AND pesel = '90010112345';
   END test_add_student_success;
+
+  --TEST TO DELETE A STUDENT RECORD
+  PROCEDURE test_delete_student_success IS
+    v_student_count_before NUMBER;
+    v_student_count_after NUMBER;
+    v_student_id NEO.students.student_id%TYPE;
+  BEGIN
+
+    SELECT COUNT(*) INTO v_student_count_before FROM NEO.students;
+
+    NEO.pkg_student_management.add_new_student(
+      v_first_name => 'John',
+      v_last_name => 'Doe',
+      v_email => 'john.doe@example.com',
+      v_city => 'SampleCity',
+      v_street => 'SampleStreet',
+      v_home_number => '12',
+      v_postal_code => '12345',
+      v_phone_number => '123456789',
+      v_pesel => '90010112345'
+    );
+
+    SELECT student_id 
+    INTO v_student_id 
+    FROM NEO.students 
+    WHERE phone_number = '123456789' AND pesel = '90010112345';
+
+    SELECT COUNT(*) INTO v_student_count_after FROM NEO.students;
+
+    ut.expect(v_student_count_after).to_equal(v_student_count_before + 1);
+
+    NEO.pkg_student_management.delete_student_record(v_student_id);
+
+    SELECT COUNT(*) INTO v_student_count_after FROM NEO.students;
+
+    ut.expect(v_student_count_after).to_equal(v_student_count_before);
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        ut.fail('Failed to find a student record with this id');
+    END;
+
+  END test_delete_student_success;
 END test_pkg_student_management;
 /
